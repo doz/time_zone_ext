3cd03c5a08218a7e9c8d656e43b610a6ec934f9c
Mike Chen <mhchen@gmail.com>
2013-05-26 00:31:05 -0400

    Add Daylight Savings Time support
    formatted_offset does not accommodate for DST since ActiveSupport::TimeZone
    needs to know the date to know whether DST applies

--- lib/time_zone_ext.rb
+++ lib/time_zone_ext.rb
@@ -7,7 +7,12 @@ module TimeZoneExt
     if format =~ /%z/i
       DateTime.strptime(date, format).in_time_zone
     else
-      DateTime.strptime("#{date} zone#{formatted_offset}", "#{format} zone%z").in_time_zone
+      time_in_zone = DateTime.strptime("#{date} zone#{formatted_offset}", "#{format} zone%z").in_time_zone
+      if time_in_zone.formatted_offset != formatted_offset
+        DateTime.strptime("#{date} zone#{time_in_zone.formatted_offset}", "#{format} zone%z").in_time_zone
+      else
+        time_in_zone
+      end
     end
   end
 end
