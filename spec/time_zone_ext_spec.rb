413455eb636775dd42b23f4416cbdd1c9e6809b8
Marcos G. Zimmermann <mgzmaster@gmail.com>
2014-07-10 16:14:21 -0300

    ActiveSupport::TimeZone gives incorrect offset during Daylight Savings Time

--- spec/time_zone_ext_spec.rb
+++ spec/time_zone_ext_spec.rb
@@ -17,8 +17,22 @@ describe TimeZoneExt do
     Time.zone.strptime("2012-06-02 00:00", "%Y-%m-%d %H:%M").to_s.should == "2012-06-02 00:00:00 -0500"
   end
 
-  it "parses time with named time zone" do
-    Time.zone = "Moscow"
-    Time.zone.strptime("2012-06-02 00:00", "%Y-%m-%d %H:%M").to_s.should == "2012-06-02 00:00:00 +0400"
+  describe "Central Time (US & Canada)" do
+    before(:each) do
+      Time.zone = 'Central Time (US & Canada)'
+    end
+
+    describe "Daylight Savings Time" do
+      it { expect(Time.zone.now.strftime('%:z')).to eq('-05:00') }
+      it { expect(Time.zone.strptime("2014-07-20 12:00", "%Y-%m-%d %H:%M").to_s).to eq('2014-07-20 12:00:00 -0500') }
+    end
+
+    describe "Regular Time" do
+      before do
+        Timecop.freeze(Time.local(2014,1,15,12,0))
+      end
+      it { expect(Time.zone.now.strftime('%:z')).to eq('-06:00') }
+      it { expect(Time.zone.strptime("2014-01-20 12:00", "%Y-%m-%d %H:%M").to_s).to eq('2014-01-20 12:00:00 -0600') }
+    end
   end
 end
