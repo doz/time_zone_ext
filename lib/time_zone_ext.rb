2660b552de4bfc8d4184a08040ee1c61c937db4f
Prince <prince@mobomo.com>
2016-08-28 22:52:35 +0530

    Adding support for DST times.

--- lib/time_zone_ext.rb
+++ lib/time_zone_ext.rb
@@ -7,7 +7,8 @@ module TimeZoneExt
     if format =~ /%z/i
       DateTime.strptime(date, format).in_time_zone
     else
-      DateTime.strptime("#{date} zone#{formatted_offset}", "#{format} zone%z").in_time_zone
+      parsed_time = Time.strptime(date_time_string, format)
+      ActiveSupport::TimeWithZone.new(nil, Time.zone, parsed_time)
     end
   end
 end
