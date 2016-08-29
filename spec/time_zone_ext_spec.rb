5258e188bc72a147b30b75fdc4fa6d3199ab4b88
prince <prince@mobomo.com>
2016-08-29 19:24:56 +0530

    Changing specs to use expect in favour of deprecated should

--- spec/time_zone_ext_spec.rb
+++ spec/time_zone_ext_spec.rb
@@ -6,11 +6,13 @@ describe TimeZoneExt do
   end
 
   it "parses time with timezone specified by name" do
-    Time.zone.strptime("2012-06-02 00:00 UTC", "%Y-%m-%d %H:%M %Z").to_s.should == "2012-06-01 19:00:00 -0500"
+    time = Time.zone.strptime("2012-06-02 00:00 UTC", "%Y-%m-%d %H:%M %Z")
+    expect(time.to_s).to eq("2012-06-01 19:00:00 -0500")
   end
 
   it "parses time with timezone specified by offset" do
-    Time.zone.strptime("2012-06-02 00:00 +0100", "%Y-%m-%d %H:%M %z").to_s.should == "2012-06-01 18:00:00 -0500"
+    time = Time.zone.strptime("2012-06-02 00:00 +0100", "%Y-%m-%d %H:%M %z")
+    expect(time.to_s).to eq("2012-06-01 18:00:00 -0500")
   end
 
   describe 'time without explicit timezone' do
@@ -39,6 +41,7 @@ describe TimeZoneExt do
 
   it "parses time with named time zone" do
     Time.zone = "Moscow"
-    Time.zone.strptime("2012-06-02 00:00", "%Y-%m-%d %H:%M").to_s.should == "2012-06-02 00:00:00 +0400"
+    time = Time.zone.strptime("2012-06-02 00:00", "%Y-%m-%d %H:%M")
+    expect(time.to_s).to eq("2012-06-02 00:00:00 +0400")
   end
 end
